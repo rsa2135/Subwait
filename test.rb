b3f9f05cbe381256da1b09ced6a3b152a3b511fc
rsa2135 <ronariav@gmail.com>
2017-05-16 22:05:07 -0400

    Add navbar

---
+++ test.rb
@@ -0,0 +1,36 @@
+require 'byebug'
+def sort3(strings, length)
+  debugger
+  (length - 1).downto(0) do |i|
+    buckets = Array.new(26) { [] }
+    strings.each do |string|
+      letter = string[i]
+      buckets[letter.ord - "a".ord] << string
+    end
+
+    strings = []
+    buckets.each do |bucket|
+      bucket.each { |string| strings << string }
+    end
+  end
+
+  p strings
+end
+
+
+
+
+def weighted_random_index(arr)
+  debugger
+  total_sum = arr.inject(:+)
+  value = rand(total_sum)
+
+  cumulative_sum = 0
+  arr.each_with_index do |el, i|
+    cumulative_sum += el
+    return i if value < cumulative_sum
+  end
+end
+
+a = [4, 6, 8, 4]
+weighted_random_index(a)
